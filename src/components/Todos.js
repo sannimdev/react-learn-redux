bc064d8fada341d039f9fc29bcf286eacac09b08
Sannim <inegg.apps@gmail.com>
2020-07-27 18:09:11 +0900

    Todos 프레젠테이셔널 컴포넌트 만들기

---
+++ src/components/Todos.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function TodoItem({ todo, onToggle }) {
+  return (
+    <li
+      style={{
+        textDecoration: todo.done ? "line-through" : "none",
+      }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+}
+
+function TodoList({ todos, onToggle }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />
+      ))}
+    </ul>
+  );
+}
+
+function Todos({ todos, onCreate, onToggle }) {
+  //리덕스를 사용한다고 해서 모든 상태를 리덕스로만 관리할 필요는 없다.
+  const [text, setText] = useState("");
+  const onChange = (e) => setText(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onCreate(text);
+    setText("");
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={text} onChange={onChange} placeholder="할 일을 입력하세요..." />
+        <button type="submit">등록</button>
+      </form>
+      <TodoList todos={todos} onToggle={onToggle} />
+    </div>
+  );
+}
+
+export default Todos;
